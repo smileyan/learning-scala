c4b684e0fe3879e84cd71275e151e8d3a3d05995
smileyan <papayado@gmail.com>
2015-12-29 08:02:53 +1030

    Adding ActorDriver

--- src/main/scala/introscala/shapes/ShapesActorDriver.scala
+++ src/main/scala/introscala/shapes/ShapesActorDriver.scala
@@ -1,3 +1,34 @@
 package introscala.shapes
 import akka.actor.{Props, Actor, ActorRef, ActorSystem}
 import com.typesafe.config.ConfigFactory
+
+private object Start
+
+object ShapesActorDriver {
+  def main(args: Array[String]) = {
+    val system = ActorSystem("DrawingActorSystem", ConfigFactory.load())
+    val drawer = system.actorOf(
+      Props(new ShapesDrawingActor ), "drawingActor")
+    val driver = system.actorOf(
+      Props(new ShapesActorDriver(drawer) ), "drawingService")
+    driver ! Start
+  }
+}
+
+class ShapesActorDriver(drawerActor: ActorRef) extends Actor {
+  import Messages._
+
+  def receive = {
+    case Start =>
+      drawerActor ! Circle(Point(0.0, 0.0), 1.0)
+      drawerActor ! 314
+      drawerActor ! Exit
+    case Finished =>
+      println(s"ShapesActorDriver cleaning up...")
+      context.system.shutdown()  
+    case response: Response =>
+      println("ShapesActorDriver Response:" + response)  
+    case unexcepted =>
+      println("ub")  
+  }
+}
