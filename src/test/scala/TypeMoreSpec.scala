954cdf2e465e12353464620e09c7c5db2ac45729
smileyan <papayado@gmail.com>
2015-12-30 08:09:14 +1030

    Adding type spec

---
+++ src/test/scala/TypeMoreSpec.scala
@@ -0,0 +1,20 @@
+package scala.school
+
+import org.specs2._
+
+object TypeMore extends org.specs2.mutable.Specification {
+  // "Semicolons"
+  // Variable Declarations
+  // Ranges
+  // Partial Functions
+  // MatchError
+  // pf1 orElse pf2 ...
+
+  // Method Declaratins
+  // Method default and named Arguments
+
+  // Methods with multiple Argument Lists
+  // A Taste of Futures
+
+  // Nested Method Definitions and Recursion
+}
