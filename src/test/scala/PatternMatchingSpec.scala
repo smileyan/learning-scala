5a6998893985f4a114f83c0324f31037007bafa1
smileyan <papayado@gmail.com>
2016-01-07 08:11:34 +1030

    adding patter matching

--- src/test/scala/PatternMatchingSpec.scala
+++ src/test/scala/PatternMatchingSpec.scala
@@ -92,4 +92,90 @@ object PatternMatchingSpec extends org.specs2.mutable.Specification {
 
     1 must_== 1
   }
+  "Matching on Tuples" >> {
+
+    val langs = Seq(
+      ("Scala", "m", "o"),
+      ("Clojure", "r", "h"),
+      ("Lisp", "j", "m"))
+
+    for (tuple <- langs) {
+      tuple match {
+        case ("Scala", _, _) => println("Found Scala")
+        case (lang, first, last) =>
+          println(s"Found other lan: $lang ($first, $last)")
+      }
+    }
+
+    1 must_== 1
+  }
+  "Guards in case Clauses" >> {
+    for( i <- Seq(1,2,3,4)) {
+      i match {
+        case _ if i%2 == 0 => println(s"even: $i")
+        case _             => println(s"odd: $i")
+      }
+    }
+
+    1 must_== 1
+  }
+  "Matching on case Classes" >> {
+    case class Address(street: String, city: String, country: String)
+    case class Person(name: String, age: Int, address: Address)
+
+    val alice = Person("Alice", 25, Address("1 s", "c", "USA"))
+    val bob = Person("Bob", 29, Address("2 j", "m", "USA"))
+    val charlie = Person("Charlie", 32, Address("3 p", "b", "USA"))
+
+    for( person <- Seq(alice, bob, charlie)) {
+      person match {
+        case Person("Alice", 25, Address(_, "c", _)) => println("Hi Alice!")
+        case Person("Bob", 29, Address("2 j", "m", "USA")) => println("Hi Bob!")
+        case Person(name, age, _) => println(s"$age $name")
+      }
+    }
+
+    val itemCosts = Seq(("pen", 0.52), ("paper", 1.35), ("note", 2.43))
+    val itemCostsIndices = itemCosts.zipWithIndex
+    for( itemCostIndex <- itemCostsIndices) {
+      itemCostIndex match {
+        case ((item, cost), index) => println(s"$index: $item $cost")
+      }
+    }
+
+    def processSeq2[T](l: Seq[T]): Unit = l match {
+      case +:(head, tail) => 
+        print("%s +: ", head)
+        processSeq2(tail)
+      case Nil => print("Nil")
+    }
+
+    case class With[A, B](a: A, b: B)
+
+    val with1: With[String, Int] = With("foo", 1)
+    val with2: String With Int = With("bar", 2)
+
+    Seq(with1, with2) foreach { w =>
+      w match {
+        case s With i => println(s"$s with $i")
+        case _ => println(s"un $w")
+      }
+      
+
+    }
+
+    val nonEmptyList = List(1,2,3,4,5)
+    val nonEmptyVector = Vector(1,2,3,4,5)
+    val nonEmptyMap = Map("one" -> 1, "two" -> 2,"three" -> 3)
+
+    def reverseSeqToString[T](l: Seq[T]): String = l match {
+      case prefix :+ end => reverseSeqToString(prefix) + s" :+ $end"
+      case Nil => "Nil"
+    }
+
+    for( seq <- Seq(nonEmptyList, nonEmptyVector, nonEmptyMap.toSeq)) {
+      println(reverseSeqToString(seq))
+    }
+    1 must_== 1
+  }
 }
