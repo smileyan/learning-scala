06e97d5ee13c0c78ab3eecdc09484b77eb3ea57a
smileyan <papayado@gmail.com>
2016-01-20 08:16:15 +1030

    Adding class pattern

--- src/test/scala/ImplicitsSpec.scala
+++ src/test/scala/ImplicitsSpec.scala
@@ -175,4 +175,33 @@ object ImplicitsSpec extends org.specs2.mutable.Specification {
     val jsonobj = json"{name: $name1, book: $book}"
     jsonobj.obj("name") must_== "De Wa"
   }
+  "Type Class Pattern" >> {
+    case class Address(street: String, city: String)
+    case class Person(name: String, address: Address)
+
+    // trait ToJSON {
+    //   def toJSON(level: Int = 0): String
+
+    //   val INDENTATION = "  "
+    //   def indentation(level: Int = 0): (String, String) = 
+    //     (INDENTATION * level, INDENTATION * (level + 1))
+    // }
+
+    implicit class AddressToJSON(address: Address) { //extends ToJSON {
+      val INDENTATION = "  "
+      def indentation(level: Int = 0): (String, String) = 
+        (INDENTATION * level, INDENTATION * (level + 1))
+      def toJSON(level: Int = 0): String = {
+        val (outdent, indent) = indentation(level)
+        s"""{
+          |${indent}"street": "${address.street}",
+          |${indent}"city":   "${address.city}"
+          |$outdent}""".stripMargin
+      }
+    }
+
+    val a = Address("1 Scala Lane", "Anytown")
+    println(a.toJSON())
+    1 must_== 1
+  }
 }
