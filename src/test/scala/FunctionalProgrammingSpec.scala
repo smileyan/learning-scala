3293dda4e22d5ae004981851fb7268b1d4475e42
smileyan <papayado@gmail.com>
2016-01-21 11:58:17 +1030

    Adding fp

--- src/test/scala/FunctionalProgrammingSpec.scala
+++ src/test/scala/FunctionalProgrammingSpec.scala
@@ -28,5 +28,48 @@ object FunctionalProgrammingSpec extends org.specs2.mutable.Specification {
     }
 
     m1(m2) must_== 122880
+
+    def factorial(i: BigInt): BigInt = {
+      if (i == 1) i
+      else i * factorial(i - 1)
+    }
+
+    factorial(3) must_== 6
+  }
+  "Tail Calls and Tail-Call Optimization" >> {
+    import scala.annotation.tailrec
+
+    def factorial(i: BigInt): BigInt = {
+      @tailrec
+      def fact(i: BigInt, accumulator: BigInt): BigInt =
+        if(i == 1){
+          accumulator
+        } else {
+          fact(i - 1, i * accumulator)
+        }
+
+      fact(i, 1)
+    }
+    factorial(3) must_== 6
+  }
+  // "Trampoline for Tail Calls" >> {
+  //   import scala.util.control.TailCalls._
+
+  //   def isEven(xs: List[Int]): TailRec[Boolean] =
+  //     if (xs.isEmpty) done(true) else tailcall(isOdd(xs.tail))
+
+  //   def isOdd(xs: List[Int]): TailRec[Boolean] =
+  //     if (xs.isEmpty) done(false) else tailcall(isEven(xs.tail))
+
+  //   isEven((1 to 1).toList).result must_== false
+
+  // }
+  "Partially Applied Functions Versus Partial Functions" >> {
+    def cat1(s1: String)(s2: String) = s1 + s2
+    cat1("Hello, ")("world!") must_== "Hello, world!"
+
+    val hello = cat1("Hello, ")_ // <function1>
+    hello("world!") must_== "Hello, world!"
+
   }
 }
