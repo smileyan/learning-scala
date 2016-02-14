a0c7051fc2011ed3dc3dbee8ddfc6e8f28779805
smileyan <papayado@gmail.com>
2016-02-15 08:12:23 +1030

    Adding covariance ...

--- src/test/scala/os/Variance.scala
+++ src/test/scala/os/Variance.scala
@@ -8,6 +8,11 @@ object Variance extends org.specs2.mutable.Specification {
     class CSuper             { def msuper() = println("CSuper") }
     class C extends CSuper   { def m() = println("C") }
     class CSub extends C     { def msub() = println("Sub") }
+
+    var f: C => C = (c: C)      => new C
+        f         = (c: CSuper) => new CSub
+//        f         = (c: CSub)   => new CSuper
+  //    Function1[C-, C+]
     1 must_== 1
   }
 }
