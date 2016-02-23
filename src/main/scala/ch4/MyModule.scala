d5c499f7047e72151eec66558ffd309eb18cb04e
smileyan <papayado@gmail.com>
2016-02-24 08:06:50 +1030

    Adding ch4

---
+++ src/main/scala/ch4/MyModule.scala
@@ -0,0 +1,41 @@
+package ch4
+
+/**
+  * Created by huay on 23/02/2016.
+  */
+class MyModule {
+  def failingFn(i: Int): Int = {
+    val y: Int = throw new Exception("fail!")
+    try {
+      val x = 42 + 5
+      x + y
+    }
+    catch { case e: Exception => 43}
+  }
+
+  def failingFn2(i: Int): Int = {
+    try {
+      val x = 42 + 5
+      x + ((throw new Exception("fail!")): Int)
+    }
+    catch { case e: Exception => 43 }
+  }
+
+  // partial function
+  def mean_0(xs: Seq[Double]): Double =
+    if (xs.isEmpty)
+      throw new ArithmeticException("mean of empty list!")
+    else xs.sum / xs.length
+
+  def mean_1(xs: IndexedSeq[Double], onEmpty: Double): Double =
+    if (xs.isEmpty) onEmpty
+    else xs.sum / xs.length
+
+  def mean_2(xs: Seq[Double]): Option[Double] =
+    if (xs.isEmpty) None
+    else Some(xs.sum / xs.length)
+
+  def variance(xs: Seq[Double]): Option[Double] =
+    mean_2(xs) flatMap (m => mean_2(xs.map(x => math.pow(x - m, 2))))
+
+}
