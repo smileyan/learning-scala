44736b46da4af424b3873737246936a8963cf203
smileyan <papayado@gmail.com>
2016-02-29 08:13:35 +1030

    Adding stream

--- src/main/scala/ch5/Stream.scala
+++ src/main/scala/ch5/Stream.scala
@@ -1,9 +1,31 @@
 package ch5
 
+import Stream._
 /**
   * Created by huay on 27/02/2016.
   */
-sealed trait Stream[+A]
+sealed trait Stream[+A] {
+  def headOption: Option[A] = this match {
+    case Empty => None
+    case Cons(h, t) => Some(h())
+  }
+
+  def toList: List[A] = this match {
+    case Empty => Nil
+    case Cons(h, t) => h()::t().toList
+  }
+
+  def take(n: Int): Stream[A] = this match {
+    case Empty => Empty
+    case Cons(h, t) if (n == 1) => cons(h(),empty)
+    case Cons(h, t) if (n > 1) => cons(h(),t().take(n-1))
+  }
+
+  def drop(n: Int): Stream[A] = this match {
+    case Empty => Empty
+    case Cons(h,t) if (n>0) => t().drop(n-1)
+  }
+}
 case object Empty extends Stream[Nothing]
 case class Cons[+A](h: () => A, t: () => Stream[A]) extends Stream[A]
 
@@ -12,7 +34,7 @@ object Stream {
     lazy val head = hd
     lazy val tail = tl
 
-    return Cons(() => head, () => tail)
+    Cons(() => head, () => tail)
   }
 
   def empty[A]: Stream[A] = Empty
@@ -23,4 +45,5 @@ object Stream {
     else
       cons(as.head, apply(as.tail: _*))
   }
+
 }
