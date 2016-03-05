ffde306ff1abdf0e6ca8bad8db9847e41b4075c8
smileyan <papayado@gmail.com>
2016-03-06 08:19:09 +1030

    Adding map2

--- src/main/scala/ch6/RNG.scala
+++ src/main/scala/ch6/RNG.scala
@@ -80,4 +80,17 @@ object RNG {
 
   def double_c: Rand[Double] =
     map(nonNegativeInt)(i => i / (Int.MaxValue.toDouble + 1))
+
+  def map2[A,B,C](ra: Rand[A], rb: Rand[B])(f: (A,B) => C): Rand[C] =
+    rng => {
+      val (a, r1) = ra(rng)
+      val (b, r2) = rb(r1)
+      (f(a,b), r2)
+    }
+
+  def both[A,B](ra: Rand[A], rb: Rand[B]): Rand[(A,B)] =
+    map2(ra,rb)((a: A,b: B) => (a,b))
+
+  val randIntDouble: Rand[(Int,Double)] = both(int,double_c)
+  val randDoubleInt: Rand[(Double,Int)] = both(double_c,int)
 }
