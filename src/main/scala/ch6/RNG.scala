aadb72f5c3b2cd0b71be4f2559d8a082b5790d64
smileyan <papayado@gmail.com>
2016-03-09 08:25:03 +1030

    Adding par ...

--- src/main/scala/ch6/RNG.scala
+++ src/main/scala/ch6/RNG.scala
@@ -104,7 +104,7 @@ case class State[S,+A](run: S => (A,S)) {
   def map[B](f: A => B): State[S, B] =
     flatMap(a => unit(f(a)))
 
-  def flatMap[B](f: A => State[S, B]) =
+  def flatMap[B](f: A => State[S, B]): State[S, B] =
     State(s => {
       val (a,s1) = run(s)
       f(a).run(s1)
