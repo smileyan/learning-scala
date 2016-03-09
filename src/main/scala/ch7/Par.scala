bae04d5b090d793e12e42dd92149cf00378f6f9f
smileyan <papayado@gmail.com>
2016-03-10 08:18:35 +1030

    Adding par ...

---
+++ src/main/scala/ch7/Par.scala
@@ -0,0 +1,39 @@
+package ch7
+
+import java.util.concurrent.{Callable, TimeUnit, Future, ExecutorService}
+
+/**
+  * Created by huay on 8/03/2016.
+  */
+
+object Par {
+
+  type Par[A] = ExecutorService => Future[A]
+
+  def unit[A](a: A): Par[A] = (es: ExecutorService) => UnitFuture(a)
+
+  private case class UnitFuture[A](get: A) extends Future[A] {
+    def isDone = true
+    def get(timeout: Long, units: TimeUnit) = get
+    def isCancelled = false
+    def cancel(evenIfRunning: Boolean): Boolean = false
+  }
+
+  def get[A](a: Par[A]): A = ???
+
+  def map2[A,B,C](a: Par[A], b: Par[B])(f: (A,B) => C): Par[C] =
+    (es: ExecutorService) => {
+      val af = a(es)
+      val bf = b(es)
+      UnitFuture(f(af.get, bf.get))
+    }
+
+  def fork[A](a: => Par[A]): Par[A] =
+    es => es.submit(new Callable[A] {
+      override def call(): A = a(es).get
+    })
+
+  def lazyUnit[A](a: => A): Par[A] = fork(unit(a))
+
+  def run[A](s: ExecutorService)(a: Par[A]): Future[A] = a(s)
+}
