31f1f7296516c7c15b2ee8104df2b1a166a9c2ad
smileyan <papayado@gmail.com>
2016-04-02 10:15:12 +1030

    Adding foldmap

--- src/main/scala/ch10/Monoids.scala
+++ src/main/scala/ch10/Monoids.scala
@@ -86,4 +86,15 @@ object Monoid {
   // op(a, op(b, op(c,d)))
   // op(op(op(a,b),c),d)
   // op(op(a,b),op(c,d))
+
+  def foldMapV[A, B](as: IndexedSeq[A], m: Monoid[B])(f: A => B): B = {
+    if (as.length == 0) {
+      m.zero
+    } else if (as.length == 1) {
+      f(as(0))
+    } else {
+      val (l, r) = as.splitAt(as.length / 2)
+      m.op(foldMapV(l, m)(f), foldMapV(r, m)(f)))
+    }
+  }
 }
