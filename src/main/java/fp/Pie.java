6ea80b235a49b8a5282e4257183e6201490d1c0e
smileyan <papayado@gmail.com>
2016-04-21 08:21:10 +0930

    Adding vistor

--- src/main/java/fp/Pie.java
+++ src/main/java/fp/Pie.java
@@ -5,13 +5,20 @@ package fp;
  */
 abstract class PieD {
     RemAV raFn = new RemAV();
+    RemFishV rfFn = new RemFishV();
     abstract PieD remA();
+    abstract PieD remFish(FishD f);
 }
 
 class Bot extends PieD {
     @Override
     PieD remA() {
         return raFn.forBot();
+    }
+
+    @Override
+    PieD remFish(FishD f) {
+        return rfFn.forBot(f);
     }
 }
 
@@ -27,13 +34,45 @@ class Top extends PieD {
     PieD remA() {
         return raFn.forTop(t,r);
     }
+
+    @Override
+    PieD remFish(FishD f) {
+        return rfFn.forTop(t,r,f);
+    }
 }
 
 abstract class FishD {}
 
-class Anchovyv extends FishD {}
+class Anchovyv extends FishD {
+    @Override
+    public boolean equals(Object obj) {
+        return (obj instanceof Anchovyv);
+    }
+}
 
-class Salmon   extends FishD {}
+class Salmon   extends FishD {
+    @Override
+    public boolean equals(Object obj) {
+        return (obj instanceof Salmon);
+    }
+}
 
-class Tuna     extends FishD {}
+class Tuna     extends FishD {
+    @Override
+    public boolean equals(Object obj) {
+        return (obj instanceof Tuna);
+    }
+}
 
+class RemFishV {
+    PieD forBot(FishD f) {
+        return new Bot();
+    }
+
+    PieD forTop(Object t, PieD r, FishD f) {
+        if(f.equals(t))
+            return r.remFish(f);
+        else
+            return new Top(t, r.remFish(f));
+    }
+}
